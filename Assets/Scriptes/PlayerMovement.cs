2e6ec93aefee40c7854304b3d60a7a16aa9f8b4b
Cephas Zulu <48860145+cazterk@users.noreply.github.com>
2021-01-06 15:21:50 +0200

    added ground check and direction flip

--- Assets/Scriptes/PlayerMovement.cs
+++ Assets/Scriptes/PlayerMovement.cs
@@ -8,12 +8,19 @@ public class PlayerMovement : MonoBehaviour
     Rigidbody2D rb;
     public float moveSpeed = 3f;
     public float jumpForce = 4f;
+    private bool facingRight;
+
     private Animator animator;
     private string currentState;
+
 
     public const string RIGHT = "right";
     public const string LEFT = "left";
     public const string JUMP = "jump";
+
+    public bool isGrounded;
+    public Transform groundCheck;
+    [SerializeField]private LayerMask groundLayer;
 
     //animation states
     const string PLAYER_IDLE = "player_idle";
@@ -21,8 +28,7 @@ public class PlayerMovement : MonoBehaviour
     const string PLAYER_JUMP = "player_jump";
     const string PLAYER_DUCK = "player_duck";
 
-    [SerializeField]
-    private LayerMask groundLayer;
+    
 
     string buttonPressed;
 
@@ -30,6 +36,7 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        facingRight = true;
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
     }
@@ -69,43 +76,70 @@ public class PlayerMovement : MonoBehaviour
 
     }
 
+
+
     //Put physica based movement in here
     private void FixedUpdate()
     {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+
+
         if (buttonPressed == RIGHT)
         {
-            rb.AddForce(new Vector2(moveSpeed, 0));
+            rb.AddForce(new Vector2(  moveSpeed, 0));
             changeAnimationState(PLAYER__RUN);
             Debug.Log("right");
+           
+            
         }
         else if (buttonPressed == LEFT)
         {
-            rb.AddForce(new Vector2(-moveSpeed, 0));
+            rb.AddForce(new Vector2(-moveSpeed , 0));
             Debug.Log("left");
             changeAnimationState(PLAYER__RUN);
+            
+
         }
         else if (buttonPressed == JUMP)
         {
-            rb.AddForce(Vector2.up * jumpForce);
-            Debug.Log("jump");
-            changeAnimationState(PLAYER_JUMP);
+            if(isGrounded)
+            {
+                rb.AddForce(Vector2.up * jumpForce);
+                Debug.Log("jump");
+                changeAnimationState(PLAYER_JUMP);
+            }
+
+            
         }
         else
         {
             changeAnimationState(PLAYER_IDLE);
         }
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+        Flip(horizontal);
     }
 
-    public LayerMask GroundLayer
+    private void HandleMovenment(float horizontal)
     {
-        get
+       
+    }
+
+    private void Flip(float horizontal)
+    {
+        if (horizontal > 0 && !facingRight || horizontal < 0 && facingRight)
         {
-            return groundLayer;
+            facingRight = !facingRight;
+
+            Vector3 theScale = transform.localScale;
+
+            theScale.x *= -1;
+
+            transform.localScale = theScale;
+
         }
     }
 
-    /**public bool Isground()
-    {
-        
-    }**/
+
+
 }
