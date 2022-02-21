f97b5dda1356621594b44dc5de7b8bdc8c6e19d0
Cephas Zulu <48860145+cazterk@users.noreply.github.com>
2022-02-21 15:43:01 +0200

    add zombie functionalities

--- Assets/Scripts/Zombie.cs
+++ Assets/Scripts/Zombie.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class Zombie : Enemy
 {
+    public ProjectileBehaviour ProjectilePrefab;
+    public Transform LaunchOffset;
     // Start is called before the first frame update
     void Start()
     {
-        
+        ProjectilePrefab.timeWhenAllowedNextShoot = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+        DistanceToPlayer();
+        Shoot();
+    }
+
+    void Shoot()
+    {
+        if (CanSeePlayer(agroRange) && ProjectilePrefab.timeWhenAllowedNextShoot <= Time.time)
+        {
+            Instantiate(ProjectilePrefab, LaunchOffset.position, LaunchOffset.rotation);
+            ProjectilePrefab.timeWhenAllowedNextShoot = Time.time + ProjectilePrefab.timeBetweenShooting;
+            AudioManager.PlaySound("shoot");
+        }
     }
 }
